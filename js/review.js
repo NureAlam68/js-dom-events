479f412f7274686f3cd6f517f13d1f2911b93a56
NureAlam68 <nurealam151068@gmail.com>
2024-09-19 12:56:28 +0600

    quote js

--- js/review.js
+++ js/review.js
@@ -6,4 +6,20 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     li.innerText =  inputValue;
     container.appendChild(li);
     document.getElementById('text-area').value = ' ';
-})
+});
+
+
+// keybaord event
+
+const inputValue = document
+.getElementById("text-area")
+.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        const inputValue = document.getElementById("text-area").value;
+        const container = document.getElementById('review');
+        const li = document.createElement("li");
+        li.innerText = inputValue;
+        container.appendChild(li);
+        document.getElementById("text-area").value = "";
+    }
+});
